0b7b155e12a88a9a91d7092099ba786583882b82
Arnault DOUYERE <arnault.douyere@sii.fr>
2024-06-05 11:03:51 +0200

    P1C2

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
 import Error from './components/Error'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +15,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/Results" element={<Results />} />
+        <Route path="/Freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
